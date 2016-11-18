2a3ddb0a0b19475455d9ce11806ce2d16ce3b01d
Brad Chao <brad@brad.tw>
2016-11-18 15:05:22 +0800

    v8

--- app/src/main/java/com/example/user/brad09/MainActivity.java
+++ app/src/main/java/com/example/user/brad09/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -12,9 +11,10 @@ import java.util.LinkedList;
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private SimpleAdapter adapter;
-    private LinkedList<HashMap<String,String>> data;    // 資料集
+    private LinkedList<HashMap<String,Object>> data;    // 資料集
     private String[] from = {"title", "content", "img"};
     private int[] to = {R.id.item_tv, R.id.item_content, R.id.item_img};
+    private int[] imgs = {R.drawable.b0, R.drawable.b1, R.drawable.b2,R.drawable.b3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +29,10 @@ public class MainActivity extends AppCompatActivity {
         data = new LinkedList<>();
 
         for (int i=0; i<20; i++) {
-            HashMap<String, String> row0 = new HashMap<>();   // 一筆資料
+            HashMap<String, Object> row0 = new HashMap<>();   // 一筆資料
             row0.put(from[0], "PPAP:" + i);
             row0.put(from[1], "apple:" + i);
+            row0.put(from[2], imgs[(int)(Math.random()*4)]);
             data.add(row0);
         }
 
